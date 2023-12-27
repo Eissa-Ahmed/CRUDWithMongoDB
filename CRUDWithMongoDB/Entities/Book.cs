908116a624db0fe2e0e1ce857b6084453fd29f0d
Eissa Ahmed <essa1998ahmed@gmail.com>
2023-12-27 20:56:51 +0200

    Add project files.

---
+++ CRUDWithMongoDB/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CRUDWithMongoDB.Entities
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = null!;
+        [BsonElement("Name")]
+        public string BookName { get; set; } = null!;
+        public string? Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ CRUDWithMongoDB/Models/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUDWithMongoDB.Models
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string BookCollection { get; set; } = null!;
+    }
+}
